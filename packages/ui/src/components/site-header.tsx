154b1b4a81861c463c1af7e4c8b4cb7f0d15717e
Slava <slavik.troyan@gmail.com>
2025-02-17 00:45:54 +0700

    login link to header

--- packages/ui/src/components/site-header.tsx
+++ packages/ui/src/components/site-header.tsx
@@ -1,18 +1,18 @@
-"use client"
+"use client";
 
-import { MenuIcon } from "lucide-react"
+import { MenuIcon } from "lucide-react";
 
-import { SearchForm } from "@workspace/ui/components/search-form"
+import { SearchForm } from "@workspace/ui/components/search-form";
 
-import { Button } from "@workspace/ui/components/button"
-import { Separator } from "@workspace/ui/components/separator"
-import { useSidebar } from "@workspace/ui/components/sidebar"
+import { Button, buttonVariants } from "@workspace/ui/components/button";
+import { Separator } from "@workspace/ui/components/separator";
+import { useSidebar } from "@workspace/ui/components/sidebar";
 
 export function SiteHeader() {
-  const { toggleSidebar } = useSidebar()
+  const { toggleSidebar } = useSidebar();
 
   return (
-    <header className="fle sticky top-0 z-50 w-full items-center border-b bg-background">
+    <header className="sticky top-0 z-50 w-full items-center border-b bg-background">
       <div className="flex h-[--header-height] w-full items-center gap-2 px-4">
         <Button
           className="h-8 w-8 md:hidden"
@@ -25,9 +25,17 @@ export function SiteHeader() {
         <Separator orientation="vertical" className="mr-2 h-4 md:hidden" />
 
         <a href="/">LOGO</a>
-       
+
         <SearchForm className="w-full sm:ml-auto sm:w-auto" />
+        
+        <div>
+          <a className={buttonVariants({ variant: "default", size: "sm" })} href="/login">  
+            Login
+          </a>
+        </div>
       </div>
+
+      
     </header>
-  )
+  );
 }
